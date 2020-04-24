20164e279d7afe13b0903673a6515323e947a07d
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2020-04-24 10:23:59 -0400

    Added local storage functionality for darkmode

--- portfolio-website/src/components/Switch/Switch.jsx
+++ portfolio-website/src/components/Switch/Switch.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import './Switch.scss'
 
 const Switch = () => {
 
-    const [ makeDark, setMakeDark ] = useState("dark");
+    const [ makeDark, setMakeDark ] = useState(
+        () => JSON.parse(window.localStorage.getItem("global-theme")) || "light"
+    );
 
-
-    function toggleMode() {
-        if (makeDark === "light" ) {
+    function toggleMode(e) {
+        if (e.target.value === "light" ) {
             setMakeDark("dark")
             changeTheme();
         } else {
@@ -21,12 +22,29 @@ const Switch = () => {
         document.documentElement.classList.add(`theme-${makeDark}`);
     }
 
+    useLayoutEffect(() => {
+        const data = window.localStorage.getItem("global-theme");
+        if (data) {
+            setMakeDark(JSON.parse(data));
+            document.documentElement.className = 'js-focus-visible';
+            document.documentElement.classList.add(`theme-${makeDark}`);
+        }
+    }, []);
+
+    useLayoutEffect(() => {
+        window.localStorage.setItem("global-theme", JSON.stringify(makeDark))
+        document.documentElement.className = 'js-focus-visible';
+        document.documentElement.classList.add(`theme-${makeDark}`);
+    });
+
     return (
-        <div className="toggle" onChange={toggleMode}>
+        <div className="toggle">
             <input
                 className="switch"
                 id={`react-switch-new`}
                 type="checkbox"
+                onChange={toggleMode}
+                value={makeDark}
             />
             <label
                 className="switch__label"
